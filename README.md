3d19ce2d643d48b576f66ebb0fec7822903ec7be
Andy Wallace <andymasteroffish@gmail.com>
2019-10-02 14:36:48 -0400

    put my project in

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # UnityCommandLineArgument
 demo of accepting a command line argument in a Unity project
+
+exmaple of the code you can drop into your app:
+
+```
+	string[] arguments = Environment.GetCommandLineArgs();
+	 for (int i=0; i<arguments.Length; i++){
+	     if (arguments[i] == "-freeplay" && i<arguments.Length-1 ){
+	     	bool free_play = arguments[i+1].ToLower() == "true";
+	     }
+	 }
+```
---
+++ command_line_test/Assets/Testo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+//info from https://answers.unity.com/questions/366195/parameters-at-startup.html
+
+//to launch with coin op mode
+//open cmdtest.app/ --args coin_opp
+
+public class Testo : MonoBehaviour
+{
+
+	static string cmdInfo = "";
+ 
+ 	public TextMesh text;
+    private bool free_play = true;
+
+     void Start () 
+     {
+     	cmdInfo += "arguments:\n";
+         string[] arguments = Environment.GetCommandLineArgs();
+         for (int i=0; i<arguments.Length; i++){
+             cmdInfo += arguments[i].ToString() + "\n ";
+             if (arguments[i] == "-freeplay" && i<arguments.Length-1 ){
+             	free_play = arguments[i+1].ToLower() == "true";
+             }
+         }
+
+         if (free_play){
+         	text.text = "FREE PLAY";
+         }else{
+         	text.text = "GIMME COINS";
+         }
+     }
+     
+     void OnGUI()
+     {
+         Rect r = new Rect(5,5, 800, 500);
+         GUI.Label(r, cmdInfo);
+     }
+}
